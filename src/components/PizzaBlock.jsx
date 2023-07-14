d5b88d93ca2a98f9ae8e308042ebeb9f2598a618
Pakhotin Nikita <nikitanik40658@icloud.com>
2023-07-14 23:18:25 +0500

    up

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,27 +1,31 @@
 import React, { useState } from 'react';
 
-function PizzaBlock({ title, price }) {
+function PizzaBlock({ title, price, imageUrl, sizes, types }) {
+  const typeName = ['Тонкое', 'Традиционное'];
+
+  const [setIndex, index] = React.useState(1);
+  const onClickCat = (index) => {
+    index(index);
+  };
+
   const [count, setCount] = useState(0);
   const Set = () => {
     setCount(count + 1);
   };
   return (
     <div className="pizza-block">
-      <img
-        className="pizza-block__image"
-        src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-        alt="Pizza"
-      />
+      <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
-          <li className="active">тонкое</li>
-          <li>традиционное</li>
+          {types.map((type) => (
+            <li>{typeName[type]}</li>
+          ))}
         </ul>
         <ul>
-          <li className="active">26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes.map((size) => (
+            <li>{size}см.</li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
